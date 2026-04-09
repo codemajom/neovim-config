d0f922bef5ef92a67ce81bb8165338be62aa8d4b
codemajom <273165693+codemajom@users.noreply.github.com>
2026-04-09 11:14:01 +0200

    Setup lazy.nvim

--- init.lua
+++ init.lua
@@ -2,6 +2,22 @@
 vim.cmd("colorscheme lunaperche")
 
 --[[ Setting up package manager]]
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
 
 --[[ Configs ]]
 vim.g.mapleader = " "
@@ -72,5 +88,68 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 --[[ Setting up plugins ]]
+-- Setup lazy.nvim
+require("lazy").setup({
+    spec = {
+        {
+            "folke/which-key.nvim",
+            event = "VimEnter",
+            opts = {
+                delay = 0,
+                icons = {
+                    mappings = vim.g.have_nerd_font,
+                    keys = vim.g.have_nerd_font and {} or {
+                        Up = "<Up> ",
+                        Down = "<Down> ",
+                        Left = "<Left> ",
+                        Right = "<Right> ",
+                        C = "<C-…> ",
+                        M = "<M-…> ",
+                        D = "<D-…> ",
+                        S = "<S-…> ",
+                        CR = "<CR> ",
+                        Esc = "<Esc> ",
+                        ScrollWheelDown = "<ScrollWheelDown> ",
+                        ScrollWheelUp = "<ScrollWheelUp> ",
+                        NL = "<NL> ",
+                        BS = "<BS> ",
+                        Space = "<Space> ",
+                        Tab = "<Tab> ",
+                        F1 = "<F1>",
+                        F2 = "<F2>",
+                        F3 = "<F3>",
+                        F4 = "<F4>",
+                        F5 = "<F5>",
+                        F6 = "<F6>",
+                        F7 = "<F7>",
+                        F8 = "<F8>",
+                        F9 = "<F9>",
+                        F10 = "<F10>",
+                        F11 = "<F11>",
+                        F12 = "<F12>",
+                    },
+                },
+            },
+        },
 
+    },
+    install = { colorscheme = { "lunaperche" } },
+    ui = {
+        icons = vim.g.have_nerd_font and {} or {
+            cmd = "⌘",
+            config = "🛠",
+            event = "📅",
+            ft = "📂",
+            init = "⚙",
+            keys = "🗝",
+            plugin = "🔌",
+            runtime = "💻",
+            require = "🌙",
+            source = "📄",
+            start = "🚀",
+            task = "📌",
+            lazy = "💤 ",
+        },
+    },
+})
 
