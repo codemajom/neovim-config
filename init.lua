23ad12634bfb9935ee4119c42bebb4eb2b917944
codemajom <273165693+codemajom@users.noreply.github.com>
2026-04-16 14:42:41 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -155,10 +155,8 @@ require("lazy").setup({
             end,
         },
 
-        {
+        { -- Important keymap: open help window with g?
             "stevearc/oil.nvim",
-            ---@module 'oil'
-            ---@type oil.SetupOpts
             opts = {
                 columns = {
                     -- "icon",
@@ -168,29 +166,7 @@ require("lazy").setup({
                 },
 
                 delete_to_trash = true,
-
-                --[[ This is here to show the default keymaps
-                keymaps = {
-                    ["g?"] = { "actions.show_help", mode = "n" },
-                    ["<CR>"] = "actions.select",
-                    ["<C-s>"] = { "actions.select", opts = { vertical = true } },
-                    ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
-                    ["<C-t>"] = { "actions.select", opts = { tab = true } },
-                    ["<C-p>"] = "actions.preview",
-                    ["<C-c>"] = { "actions.close", mode = "n" },
-                    ["<C-l>"] = "actions.refresh",
-                    ["-"] = { "actions.parent", mode = "n" },
-                    ["_"] = { "actions.open_cwd", mode = "n" },
-                    ["`"] = { "actions.cd", mode = "n" },
-                    ["g~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-                    ["gs"] = { "actions.change_sort", mode = "n" },
-                    ["gx"] = "actions.open_external",
-                    ["g."] = { "actions.toggle_hidden", mode = "n" },
-                    ["g\\"] = { "actions.toggle_trash", mode = "n" },
-                },
-                --]]
             },
-            -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
             lazy = false,
         },
     },
